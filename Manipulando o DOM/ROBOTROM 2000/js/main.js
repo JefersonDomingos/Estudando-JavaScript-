238b2a2e05813cb0baf556aab68bf224ec5090c3
Jeferson Domingos <105239532+JefersonDomingos@users.noreply.github.com>
2023-01-16 23:26:24 -0300

    Add files via upload

---
+++ Manipulando o DOM/ROBOTROM 2000/js/main.js
@@ -0,0 +1,20 @@
+console.log("teste");
+
+const somar = document.querySelector("#somar");
+const subtrair = document.querySelector("#subtrair");
+const bracos = document.querySelector("#bracos");
+
+const controle = document.querySelectorAll(".controle-ajuste");
+controle.forEach((elemento) => {
+    elemento.addEventListener("click", (evento) => {
+    manipulaDados(evento.target.textContent)
+    })
+})
+
+function manipulaDados (operacao) {
+    if(operacao === "-"){
+        bracos.value = parseInt(bracos.value) - 1;
+    }else{
+        bracos.value = parseInt(bracos.value) + 1;
+    }
+};
